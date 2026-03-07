d523a9c26d6689fd1af97e980aa0002308235be2
Nath <nathalycis20@gmail.com>
2026-03-06 19:46:20 -0500

    Añadiendo clases

---
+++ src/edu/UDistrital/Avanzada/taller2/Vista/VentanaPrincipal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.UDistrital.Avanzada.taller2.Vista;
+
+/**
+ *
+ * @author natha
+ */
+public class VentanaPrincipal {
+    
+}
