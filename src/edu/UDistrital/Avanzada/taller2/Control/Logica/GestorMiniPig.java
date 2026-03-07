b3dcb11a64ffff9109a4439255832c70107e6788
Nath <nathalycis20@gmail.com>
2026-03-06 21:43:46 -0500

    Añadiendo Metodos para insertar Cerditos

--- src/edu/UDistrital/Avanzada/taller2/Control/Logica/GestorMiniPig.java
+++ src/edu/UDistrital/Avanzada/taller2/Control/Logica/GestorMiniPig.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
  * </p>
  *
  * @author nath
- * @version 1.0
+ * @version 1.1
  * @since 2026-03-07
  */
 public class GestorMiniPig {
@@ -29,6 +29,45 @@ public class GestorMiniPig {
      */
     public GestorMiniPig() {
         this.dao = new MiniPigDAO();
+    }
+    
+    /**
+     * Inserta un minipig validando duplicados por código o microchip.
+     *
+     * @param dto minipig a insertar
+     * @return {@code true} si se insertó, {@code false} si ya existía
+     * @throws SQLException si ocurre un error en BD
+     */
+    public boolean insertarSiNoExiste(MiniPigDTO dto) throws SQLException {
+        return dao.insertarSiNoExiste(dto);
+    }
+    
+    /**
+     * Inserta un minipig (si no existe) y retorna la lista actualizada.
+     * <p>
+     * "después de insertar, inmediatamente debe poderse visualizar 
+     * </p>
+     *
+     * @param dto minipig a insertar
+     * @return lista actualizada de minipigs (incluye el nuevo si se insertó)
+     * @throws SQLException si ocurre un error en BD
+     */
+    public ArrayList<MiniPigDTO> insertarSiNoExisteYRefrescar(MiniPigDTO dto) throws SQLException {
+        dao.insertarSiNoExiste(dto);
+        return dao.listaDeMiniPigs();
+    }
+    
+    /**
+     * Verifica si un minipig ya existe en BD (por código o microchip).
+     *
+     * @param dto minipig a validar
+     * @return {@code true} si existe, {@code false} si no existe
+     * @throws SQLException si ocurre un error en BD
+     */
+    public boolean existe(MiniPigDTO dto) throws SQLException {
+        if (dto == null) return false;
+        if (dao.existePorCodigo(dto.getCodigo())) return true;
+        return dao.existePorMicrochip(dto.getIdMicrochip());
     }
 
     /**
